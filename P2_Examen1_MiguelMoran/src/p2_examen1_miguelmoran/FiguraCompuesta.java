f6815fa6af08be0aaee62b67a72cf2a9d3d9812a
Hdbygdla <131492426+MiguelMoran1234@users.noreply.github.com>
2024-08-15 18:21:38 -0600

    Add files via upload

---
+++ P2_Examen1_MiguelMoran/src/p2_examen1_miguelmoran/FiguraCompuesta.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p2_examen1_miguelmoran;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author claudiacortes
+ */
+class FiguraCompuesta extends Figura {
+    private ArrayList<Figura> figuras = new ArrayList<>();
+    
+    @Override
+    double calcularArea() {
+        double AreaTotal = 0;
+        for (int i = 0; i < figuras.size(); i++) {
+            AreaTotal += figuras.get(i).calcularArea();
+        }
+        return AreaTotal;
+    }
+    
+    public void agregarFigura(Figura figura) {
+        figuras.add(figura);
+    }
+}
